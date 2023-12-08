3da566db5030e978d14390031db640d5d9ebd4ca
Eregion <wuxize1995@gmail.com>
2023-12-07 23:07:38 -0500

    create global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary-background-color: hsl(226, 43%, 10%);
+    --primary-activity-background-color: hsl(235, 46%, 20%);
+    --hover-activity-background-color: hsl(235, 45%, 61%);
+
+    --note-text-color:hsl(236, 100%, 87%);
+}
+
+  body{
+    background-color: var(--primary-background-color);
+    font-family: 'InstrumentSans', sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    color: white;
+    font-size: 18px;
+  }
+
+`;
+export default GlobalStyle;
